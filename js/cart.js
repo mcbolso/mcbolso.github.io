69aa0d5a1912c0634a12e49153a2250ab61f9c71
Mariano <mcbolso@gmail.com>
2022-11-13 21:29:32 -0300

    Entrega 6

--- js/cart.js
+++ js/cart.js
@@ -1,4 +1,8 @@
 let unitCost;
+let subtotal;
+let costoEnvio;
+
+
 
 
 document.addEventListener("DOMContentLoaded", function () {
@@ -14,14 +18,38 @@ document.addEventListener("DOMContentLoaded", function () {
                 document.getElementById("productoCarrito").innerHTML += `<td><img src="${data.articles[i].image}" style="max-height: 70px; max-width: 70px;" class="img-thumbnail"></td>
                 <td>${data.articles[i].name}</td>
                 <td class="cantArt">${data.articles[i].currency + " " + data.articles[i].unitCost}</td>
-                <td><input type="number" class="form-control" style="max-width: 70px;" value=1 id="cantidadArticulos" onchange="calcularTotal()"></td>
+                <td><input type="number" class="form-control" style="max-width: 70px;" min=1 value=1 id="cantidadArticulos" onchange="calcularTotal()"></td>
                 <td id="subtotal">${data.articles[i].unitCost}</td>
                 `
             }
 
+            calcularTotal();
 
 
+
+
+            document.getElementById("creditCardPaymentRadio").addEventListener("change", function(){
+
+                document.getElementById("creditCardNumber").disabled = false;
+                document.getElementById("creditCardSecurityCode").disabled = false;
+                document.getElementById("dueDate").disabled = false;
+                document.getElementById("bankAccountNumber").disabled = false;
+                document.getElementById("bankAccountNumber").disabled = true;
+                document.getElementById("paymentType").innerHTML = "Tarjeta de crédito"
+            })
+        
+                
         })
+
+            document.getElementById("bankingRadio").addEventListener("change", function(){
+                document.getElementById("creditCardNumber").disabled = true;
+                document.getElementById("creditCardSecurityCode").disabled = true;
+                document.getElementById("dueDate").disabled = true;
+                document.getElementById("bankAccountNumber").disabled = false;
+                document.getElementById("paymentType").innerHTML = "Transferencia bancaria"
+            })
+
+        
 
 
 })
@@ -32,6 +60,63 @@ function calcularTotal() {
 
 
     let cantidad = document.getElementById("cantidadArticulos").value;
+    subtotal = unitCost * cantidad;
     document.getElementById("subtotal").innerHTML = unitCost * cantidad;
+    document.getElementById("subtotal2").innerHTML = unitCost * cantidad;
+
+    mostrarCostoEnvio();
 
 }
+
+function mostrarCostoEnvio(){
+
+    if (document.getElementById("premium").checked){
+        costoEnvio = subtotal * 0.15;
+    } else if (document.getElementById("express").checked){
+        costoEnvio = subtotal * 0.07;
+    } else if (document.getElementById("standar").checked){
+        costoEnvio = subtotal * 0.05;
+    }
+
+    document.getElementById("shippingText").innerHTML = costoEnvio;
+    mostrarTotal();
+}
+
+function mostrarTotal(){
+
+    document.getElementById("totalCostText").innerHTML =  costoEnvio + subtotal;
+
+}
+
+
+
+    
+
+
+
+
+
+
+
+
+
+// Ejemplo de JavaScript inicial para deshabilitar el envío de formularios si hay campos no válidos
+(function () {
+    'use strict'
+  
+    // Obtener todos los formularios a los que queremos aplicar estilos de validación de Bootstrap personalizados
+    var forms = document.querySelectorAll('.needs-validation')
+  
+    // Bucle sobre ellos y evitar el envío
+    Array.prototype.slice.call(forms)
+      .forEach(function (form) {
+        form.addEventListener('submit', function (event) {
+          if (!form.checkValidity()) {
+            event.preventDefault()
+            event.stopPropagation()
+          }
+  
+          form.classList.add('was-validated')
+        }, false)
+      })
+  })()
